542c0df9f370124d83ee326e2263ffda7c4b4a9a
topshed <top_shed@yahoo.com>
2016-11-25 11:51:57 +0000

    updated example

--- treeExample.py
+++ treeExample.py
@@ -1,14 +1,15 @@
-import xmasTree as tree
+import treeZero as tree
 from time import sleep
 
 # setup our tree with the pins it's connected to:
 mytree = tree.XmasTree(21,19,26,20)
 
-mytree.led1_on() # turn on a red LED
+mytree.red1_on() # turn on a red LED
 sleep(1) # wait one second
-mytree.led1_on() # turn on another red LED
+mytree.red1_on() # turn on another red LED
 # note that the first one turns off!
-
-mytree.yellow_pulse(1,1,5) # make the yellow led glow 5 times
-
+sleep(1)
+mytree.yellow_pulse(1,1,3) # make the yellow led glow 5 times
+sleep(1)
 mytree.all_on(2) # turn all the LEDs on
+sleep(1)
